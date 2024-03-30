81659e19607754a1b9263823005d00c294707b51
PadmaAishvarya <aishvaryasakthivel@gmail.com>
2024-03-30 12:35:07 +0530

    Initialised the repository

---
+++ src/ConnectivityTest.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author sharveshsakthivel
+ */
+
+import java.sql.*;
+public class ConnectivityTest {
+    Connection c;
+    Statement s;
+    public ConnectivityTest(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c=DriverManager.getConnection("jdbc:mysql://localhost:3306/Hospital?zeroDateTimeBehavior=CONVERT_TO_NULL");
+            s=c.createStatement();
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+    
+    
+}
---
+++ src/hospitalmanagementsystem/DriverManager.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hospitalmanagementsystem;
+
+import com.sun.jdi.connect.spi.Connection;
+
+/**
+ *
+ * @author sharveshsakthivel
+ */
+class DriverManager {
+
+    static Connection getConnection(String url, String sqlname, String sqlpassword) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
